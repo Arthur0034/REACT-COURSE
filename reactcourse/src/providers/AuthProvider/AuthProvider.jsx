ab7741067a98a98ab2b4b81287e70b0aeab7c726
Arthur0034 <arthurslvpss@gmail.com>
2024-11-18 16:14:09 -0300

    commit2

--- reactcourse/src/providers/AuthProvider/AuthProvider.jsx
+++ reactcourse/src/providers/AuthProvider/AuthProvider.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react"
-import { PropTypes } from "prop-types"
+import { func, PropTypes } from "prop-types"
 import authContext from "../../contexts/authContexts"
 
 export default function AuthProvider(props) {
     const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+    useEffect(function() {
+        const isauthenticated = JSON.parse(localStorage.getItem('isAuthenticated'))
+        setIsAuthenticated(isAuthenticated)
+    }, [])
 
     return (
         <authContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
